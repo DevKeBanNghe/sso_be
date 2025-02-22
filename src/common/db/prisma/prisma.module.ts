c02ea43b2de5d40ff73bdb42080c5cbfb978b412
CuTrung <cuphanthanhtrung4112003@gmail.com>
2025-02-23 00:09:42 +0700

    chore: update more

--- src/common/db/prisma/prisma.module.ts
+++ src/common/db/prisma/prisma.module.ts
@@ -1,11 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { DateUtilModule } from 'src/common/utils/date/date-util.module';
+import { DateUtilService } from 'src/common/utils/date/date-util.service';
 
 @Global()
 @Module({
   imports: [DateUtilModule],
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [PrismaService, DateUtilService],
+  exports: [PrismaService, DateUtilService],
 })
 export class PrismaModule {}
